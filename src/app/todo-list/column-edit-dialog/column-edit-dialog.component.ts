23072ed7ac9696b38edf3605a5118953063881c0
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-12-04 16:28:37 +0300

    clean the code

--- src/app/todo-list/column-edit-dialog/column-edit-dialog.component.ts
+++ src/app/todo-list/column-edit-dialog/column-edit-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Task } from 'src/app/interface/task.model';
 import { TodoService } from 'src/app/todo.service';
 
 @Component({
