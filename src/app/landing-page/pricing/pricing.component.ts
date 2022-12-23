d18e25ffcc4d7975dd5d572bd4f095d25eb9f64a
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-12-23 17:04:26 +0300

    update ui

--- src/app/landing-page/pricing/pricing.component.ts
+++ src/app/landing-page/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pricing.component.scss'],
 })
 export class PricingComponent {
-  max = 100;
-  min = 0;
-  showTicks = false;
-  step = 1;
-  thumbLabel = false;
-  value = 0;
+  panelOpenState = false;
+  panelOpenState2 = false;
+  panelOpenState3 = false;
+  panelOpenState4 = false;
+  panelOpenState5 = false;
+  panelOpenState6 = false;
+  panelOpenState7 = false;
+  panelOpenState8 = false;
 }
