fcd85b85d4afd590d508d7400acf671bc5a5877b
arianoorqorbani <aria.noorghorbani@gmail.com>
2022-12-25 18:25:51 +0300

    add slider

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  activatedCard: string = 'boards';
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onActive(item: string) {
+    this.activatedCard = item;
   }
-
 }
